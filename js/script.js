a621bd59d6f062e4087607ea5fef513682e5f923
ManoharManu1999 <manoharmanu.k1999@gmail.com>
2026-03-13 17:04:14 +0530

    Design10 - Minor fixs

--- js/script.js
+++ js/script.js
@@ -5,11 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
 ================================ */
 
 document.querySelectorAll('.nav-links a').forEach(link => {
-    link.addEventListener('click', e => {
-        e.preventDefault();
-        const target = document.querySelector(link.getAttribute('href'));
-        target.scrollIntoView({ behavior: 'smooth' });
-    });
+  link.addEventListener('click', e => {
+
+    const href = link.getAttribute("href");
+
+    if (href.startsWith("#")) {
+      e.preventDefault();
+      const target = document.querySelector(href);
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+
+  });
 });
 
 /* ===============================
